bf77278a4e94acb1ae6be63efc4bcdda482259f9
Mitesh Masurkar <mithilesh24m@gmail.com>
2025-01-17 21:39:56 +0530

    navbar fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,61 @@
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation(); // Get the current location
+  const isResearchPage = location.pathname === "/research"; // Check if the current route is /research
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-    document.body.style.overflow = isMenuOpen ? 'auto' : 'hidden'
-  }
+    setIsMenuOpen(!isMenuOpen);
+    document.body.style.overflow = isMenuOpen ? "auto" : "hidden";
+  };
 
   return (
-    <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
+    <nav
+      className={`nav ${isResearchPage ? "blacknav" : ""} ${
+        isMenuOpen ? "open" : ""
+      }`}
+    >
       <div className="page-padding">
         <div className="container">
           <div className="nav-inner">
             <div className="nav-left">
               <Link to="/" className="nav-logo">
                 <div className="svg-embed cc-logo-word">
-                  <img src="/images/logo.png" alt="K4M2A Logo" />
+                  {isResearchPage ? (
+                    <img src="/images/logo-main-wh.svg" class="whitelogo" />
+                  ) : (
+                    <img src="/images/logo.png" alt="K4M2A Logo" />
+                  )}
                 </div>
               </Link>
               <div className="nav-menu">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/manifesto" className="nav-link">Manifesto</Link>
-                <Link to="/research" className="nav-link">Research</Link>
-                <Link to="/careers" className="nav-link">Careers</Link>
+                <Link
+                  to="/"
+                  className={`nav-link ${
+                    location.pathname === "/" ? "w--current" : ""
+                  }`}
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/manifesto"
+                  className={`nav-link ${
+                    location.pathname === "/manifesto" ? "w--current" : ""
+                  }`}
+                >
+                  Manifesto
+                </Link>
+                <Link
+                  to="/research"
+                  className={`nav-link ${isResearchPage ? "w--current" : ""}`}
+                >
+                  Research
+                </Link>
+                <Link to="/careers" target="_blank" className="nav-link">
+                  Careers
+                </Link>
               </div>
             </div>
             <div className="nav-right">
@@ -42,7 +73,7 @@ export default function Header() {
           </div>
         </div>
       </div>
-      
+
       {/* Mobile Menu */}
       <div className="nav-mobile-menu">
         <div className="nav-mobile-menu-top"></div>
@@ -50,22 +81,29 @@ export default function Header() {
           <div className="container">
             <div className="mobile-menu-inner">
               <div className="mobile-menu-fade">
-                <Link to="/" className="nav-mobile-link">Home</Link>
+                <Link to="/" className="nav-mobile-link">
+                  Home
+                </Link>
               </div>
               <div className="mobile-menu-fade">
-                <Link to="/manifesto" className="nav-mobile-link">Manifesto</Link>
+                <Link to="/manifesto" className="nav-mobile-link">
+                  Manifesto
+                </Link>
               </div>
               <div className="mobile-menu-fade">
-                <Link to="/research" className="nav-mobile-link">Research</Link>
+                <Link to="/research" className="nav-mobile-link">
+                  Research
+                </Link>
               </div>
               <div className="mobile-menu-fade">
-                <Link to="/careers" className="nav-mobile-link">Careers</Link>
+                <Link to="/careers" className="nav-mobile-link">
+                  Careers
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
-
